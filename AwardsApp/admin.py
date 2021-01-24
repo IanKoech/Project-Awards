661af24aa3285bbf51d3faf6d55963855424e9f4
Ian K <iankoech9@gmail.com>
2021-01-24 14:30:28 +0300

    Created superuser

--- AwardsApp/admin.py
+++ AwardsApp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Project, Profile, Rating
 
 # Register your models here.
+admin.register(Project)
+admin.register(Profile)
+admin.register(Rating)
