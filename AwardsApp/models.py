3bce20693c38579c9d2f73cfd8cc60f5435287d6
Ian K <iankoech9@gmail.com>
2021-01-24 12:15:32 +0300

    Added profile model

--- AwardsApp/models.py
+++ AwardsApp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    '''
+    Class creates instances of Applications users
+    '''
+    name = models.CharField(max_length=30)
+    profilepic = models.ImageField(upload_to = 'profiles/')
+    bio = models.TextField()
+    phone = models.IntegerField()
+    email = models.EmailField(max_length=30)
+
+    def __str__(self):
+        return self.name
+    
+    def saveProfile(self):
+        self.save()
+
+    def deleteProfile(self):
+        self.delete()
