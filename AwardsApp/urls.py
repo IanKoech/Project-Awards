a7e521573ed5378d25c9cb11f5aca579c693c987
Ian K <iankoech9@gmail.com>
2021-01-25 11:55:37 +0300

    Added block styles

--- AwardsApp/urls.py
+++ AwardsApp/urls.py
@@ -4,7 +4,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
+    url(r'', views.home, name='home'),
+    # url(r'register/$', views.register, name='register'),
+    # url(r'login/$', views.loginpage, name='login'),
+    # url(r'profile/update/$', views.updateprofile, name='updateprofile'),
+    # url(r'profile/home/$', views.profilepage, name='profilepage'),
+    # url(r'project/add/$', views.addproject, name="addproject"),
+    # url(r'projects/$', views.showproject, name='projects'),
+    # url(r'project/single/(\d+)$', views.oneproject, name='single'),
 ]
 
 if settings.DEBUG:
--- AwardsApp/views.py
+++ AwardsApp/views.py
@@ -9,8 +9,8 @@ from .functions import averagingrates
 from django.contrib.auth.decorators import login_required
 
 
-# Create your views here.
-@login_required(login_url= 'login/')
+# Create your views here
+@login_required(login_url= '/accounts/login/')
 def home(request):
     project = Project.objects.get(id = 1)
     projects = Project.objects.all()
@@ -23,139 +23,139 @@ def home(request):
     return render(request, 'home.html', {"projects":projects,"rates":rates,"project1":project1})
 
 
-def register(request):
+# def register(request):
 
-    form = UserCreationForm()
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
+#     form = UserCreationForm()
+#     if request.method == "POST":
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
            
-            form.save()
+#             form.save()
             
-            user = form.cleaned_data['username']
-            defaultprofile = Profile.objects.get(id = 7)
-            profilepic = defaultprofile.profilepic
-            new_profile = Profile(name=user, profilepic = profilepic)
-            new_profile.saveprofile()
+#             user = form.cleaned_data['username']
+#             defaultprofile = Profile.objects.get(id = 7)
+#             profilepic = defaultprofile.profilepic
+#             new_profile = Profile(name=user, profilepic = profilepic)
+#             new_profile.saveprofile()
     
-            return redirect('home')
+#             return redirect('home')
 
-    return render(request, 'accounts/register.html',{"form":form})
+#     return render(request, 'accounts/register.html',{"form":form})
 
 
-def loginpage(request):
+# def loginpage(request):
 
-    if request.method == "POST":
-        username = request.POST.get("username")
-        password = request.POST.get("password")
+#     if request.method == "POST":
+#         username = request.POST.get("username")
+#         password = request.POST.get("password")
 
-        user = authenticate(request, username = username, password = password)
+#         user = authenticate(request, username = username, password = password)
 
-        if user is not None:
-            login(request, user)
+#         if user is not None:
+#             login(request, user)
 
-            return redirect('home')
+#             return redirect('home')
 
-        else:
+#         else:
             
-            message = 'The username or password you entered is incorrect'
-            username = request.POST.get("username")
-            return render(request, 'accounts/login.html', {"message": message,"username":username})
+#             message = 'The username or password you entered is incorrect'
+#             username = request.POST.get("username")
+#             return render(request, 'accounts/login.html', {"message": message,"username":username})
 
 
-    return render(request, 'accounts/login.html')
+#     return render(request, 'accounts/login.html')
 
 
-def logoutpage(request):
-    pass
+# def logoutpage(request):
+#     pass
 
 
 
 
-@login_required(login_url= 'login/')
-def updateprofile(request):
-    form = profileupdate()
-    user = request.user
-    if request.method == "POST":
-        form = profileupdate(request.POST, request.FILES)
-        profile = Profile.objects.get(name=user.username)
+# @login_required(login_url= '/accounts/login/')
+# def updateprofile(request):
+#     form = profileupdate()
+#     user = request.user
+#     if request.method == "POST":
+#         form = profileupdate(request.POST, request.FILES)
+#         profile = Profile.objects.get(name=user.username)
 
-        print(form.is_valid())
-        if form.is_valid():
-            profile = Profile.objects.get(name=user.username)
-            profilepic = form.cleaned_data['profilepic']
-            profile.profilepic = profilepic        
-            profile.bio = request.POST.get("bio")
-            profile.Phone = request.POST.get("phone")
-            profile.email = request.POST.get("email")
-            profile.save()
+#         print(form.is_valid())
+#         if form.is_valid():
+#             profile = Profile.objects.get(name=user.username)
+#             profilepic = form.cleaned_data['profilepic']
+#             profile.profilepic = profilepic        
+#             profile.bio = request.POST.get("bio")
+#             profile.Phone = request.POST.get("phone")
+#             profile.email = request.POST.get("email")
+#             profile.save()
             
 
-        return redirect("profilepage")
+#         return redirect("profilepage")
     
-    return render(request,'profile/update.html', {"form":form})
+#     return render(request,'profile/update.html', {"form":form})
 
-@login_required(login_url= 'login/')
-def profilepage(request):
+# @login_required(login_url= '/accounts/login/')
+# def profilepage(request):
 
-    user = request.user
-    profile = Profile.objects.get(name=user.username)
-    projects = Project.objects.filter(User=profile)
+#     user = request.user
+#     profile = Profile.objects.get(name=user.username)
+#     projects = Project.objects.filter(User=profile)
 
-    return render (request, 'profile/home.html', {"profile":profile, "projects":projects})
+#     return render (request, 'profile/home.html', {"profile":profile, "projects":projects})
 
 
-@login_required(login_url= 'login/')
-def addproject(request):
-    form = projectaddition()
-    if request.method == "POST":
-        form = projectaddition(request.POST, request.FILES)
+# @login_required(login_url= '/accounts/login/')
+# def addproject(request):
+#     form = projectaddition()
+#     if request.method == "POST":
+#         form = projectaddition(request.POST, request.FILES)
        
         
-        if form.is_valid():
+#         if form.is_valid():
             
-            user = request.user
-            profile = Profile.objects.get(name=user.username)
-            new_project = form.save(commit=False)
-            new_project.User = profile
-            new_project.save()
+#             user = request.user
+#             profile = Profile.objects.get(name=user.username)
+#             new_project = form.save(commit=False)
+#             new_project.User = profile
+#             new_project.save()
 
-            return redirect("home")
+#             return redirect("home")
         
-    return render(request, "projects/addproject.html", {"form":form})
+#     return render(request, "projects/addproject.html", {"form":form})
 
-@login_required(login_url= 'login/')
-def showproject(request):
-    projects = Project.objects.all()
+# @login_required(login_url= '/accounts/login/')
+# def showproject(request):
+#     projects = Project.objects.all()
 
-    return render(request, "projects/display.html", {"projects":projects})
+#     return render(request, "projects/display.html", {"projects":projects})
 
 
 
-@login_required(login_url= 'login/')
-def oneproject(request, id):
+# @login_required(login_url= '/accounts/login/')
+# def oneproject(request, id):
 
-    project = Project.objects.get(id = id)
-    ratings = Rating.objects.filter(project=project) 
-    if request.method == "POST":
-        creativity = request.POST.get("creativity")
-        design = request.POST.get("design")
-        functionality = request.POST.get("functionality")
-        average = (int(creativity)+int(design)+int(functionality)) / 3
-        user = request.user
-        profile = Profile.objects.get(name = user.username)
+#     project = Project.objects.get(id = id)
+#     ratings = Rating.objects.filter(project=project) 
+#     if request.method == "POST":
+#         creativity = request.POST.get("creativity")
+#         design = request.POST.get("design")
+#         functionality = request.POST.get("functionality")
+#         average = (int(creativity)+int(design)+int(functionality)) / 3
+#         user = request.user
+#         profile = Profile.objects.get(name = user.username)
         
 
 
-        new_rating = Rating(Design=design, Usability = functionality, Content = creativity, Average= average,Rater= profile, project=project)
-        new_rating.saverating()
+#         new_rating = Rating(Design=design, Usability = functionality, Content = creativity, Average= average,Rater= profile, project=project)
+#         new_rating.saverating()
     
-    try:
-        ratings = Rating.objects.filter(project=project)
-        rates =averagingrates(ratings) 
-    except:
-        rates = ['No ratings','No ratings','No ratings','No ratings']
+#     try:
+#         ratings = Rating.objects.filter(project=project)
+#         rates =averagingrates(ratings) 
+#     except:
+#         rates = ['No ratings','No ratings','No ratings','No ratings']
 
      
 
-    return render(request, "projects/one.html", {"project":project,"rates":rates, "ratings":ratings})
+#     return render(request, "projects/one.html", {"project":project,"rates":rates, "ratings":ratings})
